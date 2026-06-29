e9daa145cadb3993513025435e73f0855885d546
Jordan <novaardent3d@gmail.com>
2026-06-28 20:20:11 -0400

    added Material RTT visual element, so that shaders can be used in editor

---
+++ Editor/Elements/Common/VisualElements/MaterialRTTVisualElement.cs
@@ -0,0 +1,115 @@
+using Unity.Collections;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+namespace JESUIS.Editor.Elements.Common.VisualElements
+{
+    public class MaterialRTTVisualElement : VisualElement
+    {
+        ushort[] indices;
+        Vertex[] vertices;
+
+        Shader shader;
+        Material material;
+        RenderTexture renderTexture;
+
+        public MaterialRTTVisualElement(Shader shader)
+        {
+            if (shader == null)
+            {
+                Debug.LogError("Shader is null");
+                return;
+            }
+
+            material = new Material(shader);
+            this.shader = shader;
+
+            generateVisualContent += OnGenerateVisualContent;
+            UpdateIndexArray();
+        }
+
+        void UpdateIndexArray()
+        {
+            indices = new ushort[6] { 0, 1, 2, 2, 3, 0 };
+        }
+
+        void UpdateVertexArray(int width, int height)
+        {
+            vertices = new Vertex[4];
+
+            float left = 0;
+            float right = width;
+            float top = 0;
+            float bottom = height;
+
+            vertices[0].position = new Vector3(left, bottom, Vertex.nearZ);
+            vertices[1].position = new Vector3(left, top, Vertex.nearZ);
+            vertices[2].position = new Vector3(right, top, Vertex.nearZ);
+            vertices[3].position = new Vector3(right, bottom, Vertex.nearZ);
+
+            vertices[0].uv = new Vector2(0, 1);
+            vertices[1].uv = new Vector2(0, 0);
+            vertices[2].uv = new Vector2(1, 0);
+            vertices[3].uv = new Vector2(1, 1);
+
+            vertices[0].tint = Color.white;
+            vertices[1].tint = Color.white;
+            vertices[2].tint = Color.white;
+            vertices[3].tint = Color.white;
+        } 
+
+        public void SetSize(int width, int height)
+        {
+            if (renderTexture != null)
+            {
+                renderTexture.Release();
+                UnityEngine.Object.DestroyImmediate(renderTexture); 
+            }
+
+            renderTexture = new RenderTexture(width, height, 0, RenderTextureFormat.ARGB32)
+            {
+                name = $"RTT_{shader.name}",
+                filterMode = FilterMode.Point,
+                wrapMode = TextureWrapMode.Clamp,
+            };
+
+            renderTexture.Create();
+
+            style.width = width;
+            style.height = height;
+
+            UpdateVertexArray(width, height);
+            UpdateTexture();
+        }
+
+        ~MaterialRTTVisualElement()
+        {
+            renderTexture?.Release();
+            UnityEngine.Object.DestroyImmediate(renderTexture);
+            UnityEngine.Object.DestroyImmediate(material);
+        }
+
+        public void UpdateTexture()
+        {
+            Graphics.Blit(Texture2D.whiteTexture, renderTexture, material);
+        }
+
+        void OnGenerateVisualContent(MeshGenerationContext ctx)
+        {
+            if (renderTexture == null)
+            {
+                return;
+            }
+
+            var nativeIndices = new NativeArray<ushort>(indices.Length, Allocator.Temp);
+            nativeIndices.CopyFrom(indices);
+            NativeSlice<ushort> nativeIndexSlice = new NativeSlice<ushort>(nativeIndices);
+
+            var nativeVertices = new NativeArray<Vertex>(vertices.Length, Allocator.Temp);
+            nativeVertices.CopyFrom(vertices);
+            NativeSlice<Vertex> nativeVerticesSlice = new NativeSlice<Vertex>(nativeVertices);
+
+            ctx.DrawMesh(nativeVerticesSlice, nativeIndexSlice, renderTexture);
+        }
+    }
+}
