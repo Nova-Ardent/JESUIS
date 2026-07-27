ad2ceac24caf7c4378e277d5b98bb22e4cac1e14
Jordan <novaardent3d@gmail.com>
2026-07-27 12:49:52 -0400

    Extended visual element functionality, to include relative deltas.

---
+++ Editor/Utilities/UnityEngine/UIElements/VisualElementExtensions.cs
@@ -0,0 +1,25 @@
+using UnityEngine;
+
+namespace UnityEngine.UIElements
+{
+    public static class VisualElementExtensions
+    {
+        public static Vector2 GetRelativeDelta(this VisualElement element, Vector2 delta)
+        {
+            Vector2 previousLocal = element.WorldToLocal(-delta);
+            Vector2 currentLocal = element.WorldToLocal(Vector2.zero);
+            return currentLocal - previousLocal;
+        }
+
+        public static Vector2 GetRelativeDelta(this VisualElement target, VisualElement source, Vector2 delta)
+        {
+            Vector2 sourcePreviousLocal = source.LocalToWorld(-delta);
+            Vector2 sourceCurrentLocal = source.LocalToWorld(Vector2.zero);
+
+            Vector2 targetPreviousLocal = target.WorldToLocal(sourcePreviousLocal);
+            Vector2 targetCurrentLocal = target.WorldToLocal(sourceCurrentLocal);
+
+            return targetCurrentLocal - targetPreviousLocal;
+        }
+    }
+}
